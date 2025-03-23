3e0aa0befc92dee476b93b3fd991f6ba1550c1fd
Pothireddy Gelivi <pothireddy@example.com>
2025-03-23 12:06:44 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO).then(() => {
     })
 
 const app = express();
-app.listen(3000, () =>{
-    console.log("server is running on port 3000 !!! ");
+app.listen(5000, () =>{
+    console.log("server is running on port 5000 !!! ");
 
 });
