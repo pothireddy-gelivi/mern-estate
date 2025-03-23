d9a434c64d1b4fdc3dd1add0e443cb4c57c1189f
Pothireddy Gelivi <pothireddy@example.com>
2025-03-23 20:51:07 +0530

    create api test route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+export const test = (req,res) => {
+    res.json({message :'api router is working!',}
+        
+    );
+    
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connect to MongoDB!');
@@ -13,3 +14,6 @@ app.listen(5000, () =>{
     console.log("server is running on port 5000 !!! ");
 
 });
+
+
+app.use("/api/user",userRouter);
